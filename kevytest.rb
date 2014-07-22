e33c2e8759986fd5cde7441deb6fc44332ddf17b
Sid Tantia <sstantia@gmail.com>
2014-07-21 21:49:06 -0400

    Back to original code - created kevynew

--- kevytest.rb
+++ kevytest.rb
@@ -1,67 +1,65 @@
-class KevyTest
-	require 'secret'
-	def compute(user_int)
-		unless user_int >= 3 # this first loop assumes that integer has to be greater than 2 for this problem otherwise there will be no prime number integer to test in secret function as no prime number exists below 2
-			puts "Please pick an integer that is greater than 2!"
-			@user_int = gets.to_i
-			
 
-		else
-			int = user_int - 1
+def compute(user_int)
+	unless user_int >= 3 # this first loop assumes that integer has to be greater than 2 for this problem otherwise there will be no prime number integer to test in secret function as no prime number exists below 2
+		puts "Please pick an integer that is greater than 2!"
+		@user_int = gets.to_i
+		
 
-			until int == 1
-				check_if_prime(int)
-				int -= 1
-			end
+	else
+		int = user_int - 1
 
-		end
-	end
-
-	def check_if_prime(int)
-		num = 2;
-		prime_flag = true;
-
-		until num >= int
-			if int % num == 0
-				prime_flag = false;
-				break
-			else
-				num += 1
-			end
-		end
-
-		if prime_flag == true
-			adder(int)
+		until int == 1
+			check_if_prime(int)
+			int -= 1
 		end
 
 	end
+end
 
+def check_if_prime(int)
+	num = 2;
+	prime_flag = true;
 
-	def adder(prime_int)
-		if @y_total 
-				@y_total += prime_int
+	until num >= int
+		if int % num == 0
+			prime_flag = false;
+			break
 		else
-			@y_total = prime_int
+			num += 1
 		end
-
-		if @secret_y_total 
-			@secret_y_total += secret(prime_int)
-		else
-			@secret_y_total = secret(prime_int)
-		end
-
 	end
 
-	def secret(y) #placeholder for calling an outside file function here
-		y = y * 2
+	if prime_flag == true
+		adder(int)
 	end
 
-	def final(y_total, secret_y_total)
-		if secret(@y_total) == @secret_y_total
-			puts "True - the secret function is additive of all prime numbers below N"
-		else
-			puts "False - the secret function is not additive of all prime numbers below N"
-		end
+end
+
+
+def adder(prime_int)
+	if @y_total 
+			@y_total += prime_int
+	else
+		@y_total = prime_int
+	end
+
+	if @secret_y_total 
+		@secret_y_total += secret(prime_int)
+	else
+		@secret_y_total = secret(prime_int)
+	end
+
+end
+
+def secret(y) #placeholder for calling an outside file function here
+	y = y * 2
+end
+
+def final(y_total, secret_y_total)
+	if secret(@y_total) == @secret_y_total
+		puts "True - the secret function is additive of all prime numbers below N"
+	else
+		puts "False - the secret function is not additive of all prime numbers below N"
 	end
 end
 
