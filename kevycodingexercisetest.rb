8a2c305f99b97d9606d68fecdc1b9ca7968bf154
Sid Tantia <sstantia@gmail.com>
2014-07-21 22:02:35 -0400

    deleted extraneous files

--- kevycodingexercisetest.rb
+++
@@ -1,8 +0,0 @@
-require 'minitest/autorun'
-require_relative 'kevycodingexercise'
-
-class KevyExerciseTest < MiniTest::Unit::TestCase
-	def test_check_if_prime_method
-		int = [3..1000]
-		int.each do ......
-		... should eq(......)
