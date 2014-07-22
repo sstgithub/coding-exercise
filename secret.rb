8277c22389934e85374e1bea2e413cc7e0e65881
Sid Tantia <sstantia@gmail.com>
2014-07-21 22:02:53 -0400

    Added explanation of why there is a 'secret.rb' file

--- secret.rb
+++ secret.rb
@@ -1,3 +1,5 @@
+#this file is just there so I can run my code in 'kevyfinal' and see if everything else works. I'm assuming that there will be a 'secret' file with the secret function in the same file path as my 'kevyfinal' program
+
 def secret(y)
 	y = y * 2
 end
