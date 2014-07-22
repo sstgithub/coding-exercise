823d677fc2ebbf2ef023fbf7d432e2694939c0b0
Sid Tantia <sstantia@gmail.com>
2014-07-21 21:48:13 -0400

    fixed typo

--- kevycodingexercise.rb
+++ kevycodingexercise.rb
@@ -13,11 +13,12 @@ require_relative 'secret' #Part of my assumptions (see below) - secret file is i
 	# 1. @user_int -- this is equal to N in the problem statement
 	# 2. int -- this represents each number below N that is tested to see if it's a prime number or not
 	# 3. num -- this represents a temporary increasing number below each integer that is being checked for 'primeness'. Each integer to be checked is divided by num and num is increased until finally num is more than or equal to integer being checked. For example: if the integer being checked was 5, then 5 would be divided by 2, then by 3, then by 4, using the modulus operator which just returns the remainder from the division. If the modulus returns 0 that means the integer being checked is not prime and it would stop there but in the case of 5, num would keep increasing until finally the loop ends because num equals the integer being checked - 5.
-	# 4. prime_flag -- this means exactly what it says...it flags a number it is prime.
+	# 4. prime_flag -- this means exactly what it says...it flags a number if it is prime.
 	# 5. @y_total and @secret_y_total -- see the adder function below
 
 	#How I could make this better:
-		# 1. When checking an integer to see if its prime, first take the square root of the integer, then only divide that integer by prime numbers that are below it. Doing both of these will significantly reduce the computation time required when dealing with very large numbers.
+		# 1. When checking an integer to see if its prime, first take the square root of the integer, then only divide that integer by prime numbers that are below it. Doing both of these will significantly reduce the computation time required when dealing with very large numbers, while still maintaining same amount of accuracy in determining whether a number is prime.
+		# 2. Provide some loading messages such as '25% complete', '50% complete' etc so user has some indication of when program will complete when dealing with very large numbers.
 
 class KevyExercise
 	def initialize
