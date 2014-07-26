563bf237faabad98aea160400ea4db035cd04a5b
Sid Tantia <sstantia@gmail.com>
2014-07-26 15:13:06 -0400

    More code cleanup
    Removed initialize, changed name of get_integer, split up long comments
    into separate lines, put comments above method they were documenting,
    reduced documentation needed by using better method names and more
    concise language

--- kevyfinal.rb
+++ kevyfinal.rb
@@ -1,15 +1,18 @@
-require_relative 'secret.rb' #assuming that there is a secret.rb file in same file path, with secret function inside it (see Readme.txt for all assumptions and function and variable definitions)
-
+# Assuming that there is a secret.rb file in same file path, with secret
+#  function inside it (see Readme.txt for all assumptions and function and
+#  variable definitions)
+require_relative 'secret.rb'
 
 class KevyFinal
-  def initialize
-  end
-
-  def get_integer
-    puts "Provide an integer for this program to use:"
+  # obtain the integer from user first and then call compute method
+  def obtain_integer
+    puts 'Provide an integer for this program to use:'
     @user_int = gets.to_i
-    while @user_int < 3 #this loop checks to make sure the given integer, user_int ('N' from the problem statement), is above 2, as otherwise there will be no prime numbers below N to test in secret function
-      puts "Please provide an integer that is greater than or equal to 3:"
+    # this loop checks to make sure the given integer, user_int ('N' from the
+    #  problem statement), is above 2, as otherwise there will be no prime
+    #  numbers below N to test in secret function
+    while @user_int < 3
+      puts 'Please provide an integer that is greater than or equal to 3:'
       @user_int = gets.to_i
     end
     compute
@@ -19,58 +22,60 @@ class KevyFinal
     int = @user_int - 1
 
     until int == 1
-      check_if_prime(int)
+      check_if_int_is_prime(int)
       int -= 1
     end
   end
 
-  def check_if_prime(int)
-    num = 2
+  def check_if_int_is_prime(int)
+    num_to_divide_by = 2
     prime_flag = true
-
-    until num >= int
-      if int % num == 0
+    until num_to_divide_by >= int
+      if int % num_to_divide_by == 0
         prime_flag = false
         break
       else
-        num += 1
+        num_to_divide_by += 1
       end
     end
-
-    #if it goes through check_if_prime all the way till int == 0, and the modulus never equals 0 that means the number is prime and prime stays flagged as true and the number needs to be added to @y_total and to @secret_y_total. So:
-
-    if prime_flag == true
-      adder(int)
-    end
-
+    # if int stays flagged as prime then it is passed to adder function so it
+    #  can be added to @y_total and @secret_y_total
+    adder(int) if prime_flag
   end
 
-
-  def adder(prime_int) # this function just adds the prime integer discovered in check_if_prime to @y_total which is equivalent to the 'x+y' in the problem statement, and it also runs the prime integer through the secret function than adds it to @secret_y_total which is the equivalent of 'secret(x) + secret(y)'' in the problem statement
-    if @y_total 
-        @y_total += prime_int
+  # @y_total is equivalent to the 'x+y' and @secret_y_total is equivalent to
+  #  'secret(x)+secret(y)' in the problem statement
+  def adder(prime_int)
+    # Adds the prime integer to @y_total
+    if @y_total
+      @y_total += prime_int
     else
       @y_total = prime_int
     end
 
-    if @secret_y_total 
+    # Runs secret function on prime integer and adds it to @secret_y_total
+    if @secret_y_total
       @secret_y_total += secret(prime_int)
     else
       @secret_y_total = secret(prime_int)
     end
-
   end
 
-  def result # this function outputs "True..." if the secret function is an additive function of all prime numbers below N, or it outputs "False..." if the secret function is not an additive function of all prime numbers below N.
+  # Outputs "True..." if the secret function is an additive function of all
+  #  prime numbers below N, or outputs "False..." if the secret function is
+  #  not an additive function of all prime numbers below N.
+  def result
     if secret(@y_total) == @secret_y_total
       puts "True - the secret function is additive of all prime numbers below #{@user_int}."
     else
       puts "False - the secret function is not additive of all prime numbers below #{@user_int}."
     end
+    # # print variables to test if rest of function worked
+    # puts @y_total, @secret_y_total
   end
 end
 
 kevy_final = KevyFinal.new
 
-kevy_final.get_integer
+kevy_final.obtain_integer
 kevy_final.result
